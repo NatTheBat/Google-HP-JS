8503ab99a23e72247b0736d8446c77a936a016f3
NatTheBat <natacha.agafonov@gmail.com>
2017-10-04 19:35:45 +0200

    ajout fenetre javascript

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,16 @@ boutonRecherche.onclick = function() {
 	let contenuRecherche = document.getElementById('recherche-google').value;
 	document.location.href='http://www.google.fr/#q=' + contenuRecherche ;
 }
+
+
+//document.onload = setTimeout(function () { alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please'); }, 10000);
+
+
+ function ouvrePopUp()
+     {
+      window.open('popup/index.html','nom_de_ma_popup','menubar=no, scrollbars=no, top=100, left=100, width=700, height=700');
+     }
+
+//document.onload = setTimeout(ouvrePopUp(), 10000);
+
+setTimeout(ouvrePopUp, 3000); 
